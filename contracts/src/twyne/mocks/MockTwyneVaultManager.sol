13e2a3c65b5928ac7e2ad1923b600f4a2a30845c
Tisca Catalin <admin@tiscacatalin.com>
2025-11-07 18:20:31 +0200

    passing tests so far & few refacto

--- contracts/src/twyne/mocks/MockTwyneVaultManager.sol
+++ contracts/src/twyne/mocks/MockTwyneVaultManager.sol
@@ -5,7 +5,7 @@
 
  /// @notice Lightweight simulation of Twyne's Vault Manager credit delegation.
  contract MockTwyneVaultManager is IVaultManager {
-     mapping(address => mapping(address => uint256)) private _delegated; // lender => borrower => assets
+     mapping(address => mapping(address => uint256)) private _delegated;
 
      function delegateBorrowingPower(address lender, address borrower, uint256 assets) external override {
          require(lender != address(0) && borrower != address(0), "ZERO_ADDRESS");
